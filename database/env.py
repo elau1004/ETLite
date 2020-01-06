e77f957ef615f57dee4d36bbf65a721c44c12783
Edward Lau <elau1004@netscape.net>
2020-01-05 20:41:21 -0800

    Added comments to abstract method

--- database/env.py
+++ database/env.py
@@ -6,6 +6,8 @@ from sqlalchemy import pool
 from alembic import context
 from sqlalchemy import MetaData
 
+# pylint: disable=no-member
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
