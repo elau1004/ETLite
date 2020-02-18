3020de4c842b57bbf40260fce4b83f37b19f0c6a
Edward Lau <elau1004@netscape.net>
2020-02-17 18:38:02 -0800

    Remove default nullable that are true.

--- alembic/database/versions/20200215_0006_create_table_status_ebc75428d24b.py
+++ alembic/database/versions/20200215_0006_create_table_status_ebc75428d24b.py
@@ -31,7 +31,8 @@ def upgrade():
         'Status'
         ,sa.Column('ID'             ,sa.SmallInteger    ,nullable=False ,comment='The Primary Key'  ,primary_key=True )
         ,sa.Column('Name'           ,sa.String(32)      ,nullable=False ,comment='Name of the severity' )
-        ,sa.Column('is_Active'      ,sa.Boolean(False)  ,nullable=False ,comment='Flag to enable/disable this row' ,server_default='TRUE' )
+        ,sa.Column('is_Active'      ,sa.Boolean(False)  ,nullable=False ,comment='Flag to enable/disable this row' ,server_default='1' )
+        ,sa.Column('is_Terminal'    ,sa.Boolean(False)  ,nullable=False ,comment='Indicate terminal state that cannot be transition out of' ,server_default='0' )
         ,sa.Column('Display_Order'  ,sa.SmallInteger    ,nullable=True  ,comment='The order to displa in UI')
         ,dt_updated_on
         #
@@ -45,45 +46,48 @@ def upgrade():
         'Status'
         ,column('ID'            ,sa.SmallInteger)
         ,column('Name'          ,sa.String(32)  )
-        ,column('isActive'      ,sa.Boolean     )
+        ,column('is_Active'     ,sa.Boolean     )
+        ,column('is_Terminal'   ,sa.Boolean     )
+        ,column('Display_Order' ,sa.SmallInteger)
+
     )
 
     op.bulk_insert(
         _table
-        ,[   {'ID': 0   ,'Name': 'Errored'      ,"is_Active": True   }
-            ,{'ID': 1   ,'Name': 'Disabled'     ,"is_Active": True   }
-            ,{'ID': 2   ,'Name': 'Enabled'      ,"is_Active": True   }
-            ,{'ID': 3   ,'Name': 'Pending'      ,"is_Active": True   }
-            ,{'ID': 4   ,'Name': 'Ready'        ,"is_Active": True   }
-            ,{'ID': 5   ,'Name': 'Cancelling'   ,"is_Active": True   }
-            ,{'ID': 6   ,'Name': 'Cancelled'    ,"is_Active": True   }
-            ,{'ID': 7   ,'Name': 'Completing'   ,"is_Active": True   }
-            ,{'ID': 8   ,'Name': 'Completed'    ,"is_Active": True   }
+        ,[   {'ID': 0   ,'Name': 'Errored'      ,"is_Active": True ,"is_Terminal": True  ,"Display_Order": 0 }
+            ,{'ID': 1   ,'Name': 'Disabled'     ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 1 }
+            ,{'ID': 2   ,'Name': 'Enabled'      ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 2 }
+            ,{'ID': 3   ,'Name': 'Pending'      ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 3 }
+            ,{'ID': 4   ,'Name': 'Ready'        ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 4 }
+            ,{'ID': 5   ,'Name': 'Cancelling'   ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 5 }
+            ,{'ID': 6   ,'Name': 'Cancelled'    ,"is_Active": True ,"is_Terminal": True  ,"Display_Order": 6 }
+            ,{'ID': 7   ,'Name': 'Completing'   ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 7 }
+            ,{'ID': 8   ,'Name': 'Completed'    ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 8 }
             #  Extraction phase
-            ,{'ID': 11  ,'Name': 'Authenticating',"is_Active":True   }
-            ,{'ID': 12  ,'Name': 'Authenticated',"is_Active": True   }
-            ,{'ID': 13  ,'Name': 'Requesting'   ,"is_Active": True   }
-            ,{'ID': 14  ,'Name': 'Requested'    ,"is_Active": True   }
-            ,{'ID': 15  ,'Name': 'Checking'     ,"is_Active": True   }
-            ,{'ID': 16  ,'Name': 'Checked'      ,"is_Active": True   }
-            ,{'ID': 17  ,'Name': 'Paginating'   ,"is_Active": True   }
-            ,{'ID': 18  ,'Name': 'Paginated'    ,"is_Active": True   }
-            ,{'ID': 19  ,'Name': 'Querying'     ,"is_Active": True   }
-            ,{'ID': 20  ,'Name': 'Queried'      ,"is_Active": True   }
-            ,{'ID': 21  ,'Name': 'Staging'      ,"is_Active": True   }
-            ,{'ID': 22  ,'Name': 'Staged'       ,"is_Active": True   }
-            ,{'ID': 23  ,'Name': 'Formatting'   ,"is_Active": True   }
-            ,{'ID': 24  ,'Name': 'Formatted'    ,"is_Active": True   }
+            ,{'ID': 11  ,'Name': 'Authenticating',"is_Active":True ,"is_Terminal": False ,"Display_Order": 11}
+            ,{'ID': 12  ,'Name': 'Authenticated',"is_Active": True ,"is_Terminal": False ,"Display_Order": 12}
+            ,{'ID': 13  ,'Name': 'Requesting'   ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 13}
+            ,{'ID': 14  ,'Name': 'Requested'    ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 14}
+            ,{'ID': 15  ,'Name': 'Checking'     ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 15}
+            ,{'ID': 16  ,'Name': 'Checked'      ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 16}
+            ,{'ID': 17  ,'Name': 'Paginating'   ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 17}
+            ,{'ID': 18  ,'Name': 'Paginated'    ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 18}
+            ,{'ID': 19  ,'Name': 'Querying'     ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 19}
+            ,{'ID': 20  ,'Name': 'Queried'      ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 20}
+            ,{'ID': 21  ,'Name': 'Staging'      ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 21}
+            ,{'ID': 22  ,'Name': 'Staged'       ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 22}
+            ,{'ID': 23  ,'Name': 'Formatting'   ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 23}
+            ,{'ID': 24  ,'Name': 'Formatted'    ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 24}
             #  Ingestion phase
-            ,{'ID': 31  ,'Name': 'Importing'    ,"is_Active": True   }
-            ,{'ID': 32  ,'Name': 'Imported'     ,"is_Active": True   }
-            ,{'ID': 33  ,'Name': 'Profiling'    ,"is_Active": True   }
-            ,{'ID': 34  ,'Name': 'Profiled'     ,"is_Active": True   }
+            ,{'ID': 31  ,'Name': 'Importing'    ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 31}
+            ,{'ID': 32  ,'Name': 'Imported'     ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 32}
+            ,{'ID': 33  ,'Name': 'Profiling'    ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 33}
+            ,{'ID': 34  ,'Name': 'Profiled'     ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 34}
             #  Certification phase
-            ,{'ID': 41  ,'Name': 'Validating'   ,"is_Active": True   }
-            ,{'ID': 42  ,'Name': 'Validated'    ,"is_Active": True   }
-            ,{'ID': 43  ,'Name': 'Failing'      ,"is_Active": True   }
-            ,{'ID': 44  ,'Name': 'Failed'       ,"is_Active": True   }
+            ,{'ID': 41  ,'Name': 'Validating'   ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 41}
+            ,{'ID': 42  ,'Name': 'Validated'    ,"is_Active": True ,"is_Terminal": True  ,"Display_Order": 42}
+            ,{'ID': 43  ,'Name': 'Failing'      ,"is_Active": True ,"is_Terminal": False ,"Display_Order": 43}
+            ,{'ID': 44  ,'Name': 'Failed'       ,"is_Active": True ,"is_Terminal": True  ,"Display_Order": 44}
         ,]
     )
 
