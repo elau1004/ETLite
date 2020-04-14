8521b454a444122fb9c26f7cc1780509c5d1aa10
Edward Lau <elau1004@users.noreply.github.com>
2020-04-13 22:54:39 -0700

    Added unit test to test Parser (#46)
    * Updated ER diagram
    
    * Added YMAL file
    
    * Added cfg variable and logger into the commons.
    
    * Fix del() bug that removed too many of the references
    
    * Tweak data model
    
    * Move parser into its own module.
    
    * Move the Lexer and parser into thier own modules
    
    * Added unit test to test DAG parser

--- etlite/common/parser.py
+++ etlite/common/parser.py
@@ -4,14 +4,14 @@
 # Licensed under the MIT License.
 #
 """
-The engine to orchestrate job executions.
+The parser to convert an input notated string into a Python collection thgat represent a DAG of j0ob execution..
 The orchestration is define as DAG of collections notated using the following brackets:
     [] - A list of jobs to be executed in sequence.
     {} - A set  of jobs to be executed in parallel threads on a single CPU core.
     () - A set  of jobs to be executed in parallel across all CPU cores.
 
 Job entries in a collection are case in-sensitive and unique in the entire DAG.
-Python modules shall be dynamicall discover and be matched to these job codes/entires.
+Python modules shall be dynamically discover, instantiated and be matched to these job codes/entires.
 Sequential processing is dependend all of previous jobs execution to be successful to proceed.
 
 Example:
@@ -24,8 +24,10 @@ import  re
 import  gettext     # Ready for future internationalization.
 i18n =  gettext.gettext
 
+from    etlite.common.lexer import  DAG_Lexer as    lexer
+
 class   DAG_Parser():
-    """The DAG parser.
+    """The DAG Parser.
     """
     # pylint: disable=W0621
     DELIMITER   = ","
@@ -38,44 +40,7 @@ class   DAG_Parser():
     MSG_TOO_MANY_BRACKET  = "Mis-match brackets!  Too many opening bracket.  Encountered '{bracket}'."
     MSG_MIS_MATCH_BRACKET = "Mis-match brackets!  Expecting '{expect}' but encountered '{actual}'."
     MSG_NON_UNIQUE_COUNT  = "Job '{job}' occured {count} times in the input DAG."
-
-
-    @staticmethod
-    def get_token( dag:str ,strPos:int ,strLen:int ) -> (str ,int):
-        """
-        Utility lexer to return a parsed token from the input string.
-        You do not need to pass in sub-string but instead the starting position to continue parsing from.
-
-        Args:
-            dag    - A string representation of a DAG.
-            strPos - Strating position to begin scanning for token.
-            strLen - Lengh of the inpiut string.
-        Return:
-            token  - A parsed token.  In out DAG language it shall be:
-                       - Left  brackets.
-                       - Right brackets.
-                       - Literal.
-            strPos - Scanned up this position in the input string.
-        Exception:
-            None
-        """
-        token = ''
-        for ch  in  dag[ strPos: ]:
-            strPos  +=  1
-            if  ch  == ',':
-                break
-
-            if  ch  not in  DAG_Parser.PUNCTUATION:
-                token   +=  ch
-
-                if  ch  in  DAG_Parser.LF_BRACKETS + DAG_Parser.RT_BRACKETS:
-                    if  strPos  +1  < strLen and dag[ strPos ] == DAG_Parser.DELIMITER:
-                        strPos  +=  1
-                    break
-                if  strPos < strLen and dag[ strPos ] in DAG_Parser.LF_BRACKETS + DAG_Parser.RT_BRACKETS:
-                    break
-
-        return  token ,strPos
+    MSG_NON_DAG_INSTANCE  = "Object not instantiated with a DAG.  Nothing to parse."
 
     @staticmethod
     def parse_dag( dag:str ,opnBkt:str=None ,strPos:int=0 ,strLen:int=None ) -> (list ,int ,str):
@@ -92,9 +57,9 @@ class   DAG_Parser():
             https://en.wikipedia.org/wiki/LALR_parser
         Args:
             dag    - A string representation of a DAG.
-            opnBkt - The opening bracket to be used to match the future closing bracket.
-            strPos - Strating position to begin scanning for token.
-            strLen - Lengh of the inpiut string.
+            opnBkt - The opening bracket to be used to match the future closing bracket.  Must not be provided for the intial invocation.
+            strPos - Starting position to begin scanning for token.  Must not be provided for the intial invocation.
+            strLen - Lengh of the input string.  Must not be oorvided for the intial invocation.
         Return:
             list   - The DAG reprented in a nested list to be used by the execution engine.
             strPos - Scanned up this position in the input string.
@@ -103,25 +68,27 @@ class   DAG_Parser():
             SyntaxError - When it doesn't conform to our DAG syntax.
         """
         # pylint: disable=R0912
-        if  not strLen:
+        if  not strLen: # NOTE: Not set on the initial call to parse the string.
+            dag    = re.sub( r"\s+" ,"" ,dag ,flags=re.UNICODE )     # Remove all spaces.
             strLen = len( dag )
 
         stack = [ opnBkt ] if opnBkt else []
         graph = []
 
         while strPos < strLen:
-            token ,strPos = DAG_Parser.get_token( dag=dag ,strPos=strPos ,strLen=strLen )
-            latkn ,_      = DAG_Parser.get_token( dag=dag ,strPos=strPos ,strLen=strLen )   # NOTE: Look ahead one token.
+            token ,strPos = lexer.next_token( dag=dag ,strPos=strPos ,strLen=strLen )
+            latkn ,_      = lexer.next_token( dag=dag ,strPos=strPos ,strLen=strLen )   # NOTE: Look ahead one token.
 
-            if  token  in DAG_Parser.LF_BRACKETS:
+            if  token  in lexer.LF_BRACKETS:
                 stack.append( token )   # Shift. Push onto the stack.
-                if  latkn not in DAG_Parser.RT_BRACKETS:
+                if  latkn not in lexer.RT_BRACKETS:
                     nested ,strPos ,delimiter = DAG_Parser.parse_dag( dag=dag ,opnBkt=stack[-1] ,strPos=strPos ,strLen=strLen )
 
                     if  len( stack ) == 0:
-                        raise   SyntaxError( i18n( DAG_Parser.MSG_TOO_MANY_BRACKET ).format( bracket=delimiter ))
+                        raise SyntaxError( i18n( DAG_Parser.MSG_TOO_MANY_BRACKET ).format( bracket=delimiter ))
+
                     if DAG_Parser.RT_BRACKETS.index( delimiter ) != DAG_Parser.LF_BRACKETS.index( stack[-1] ):
-                        raise   SyntaxError( i18n( DAG_Parser.MSG_MIS_MATCH_BRACKET ).format( expect=stack[-1] ,actual=delimiter  ))
+                        raise SyntaxError( i18n( DAG_Parser.MSG_MIS_MATCH_BRACKET ).format( expect=stack[-1] ,actual=delimiter  ))
 
                     if  len( graph ) == 0:
                         graph = nested
@@ -131,6 +98,8 @@ class   DAG_Parser():
                 return  graph ,strPos ,token
             else:
                 if  len( graph ) == 0:
+                    # NOTE: First entry is to be used to decode the type of collection
+                    #       because a set of list is NOT Python compliant.
                     if  opnBkt:
                         graph.append( opnBkt )
                     else:
@@ -173,6 +142,43 @@ class   DAG_Parser():
             raise ValueError( err_msg )
 
 
-    def __init__( self ,dag:dict ):
-        self._dag = re.sub( r"\s+" ,"" ,dag ,flags=re.UNICODE )     # Remove all spaces.
+    def __init__( self ,dag:str=None ):
+        self._dag_str:str = None
+        self._dag:list    = None
 
+        if  dag:
+            self._dag_str = dag
+
+    def parse( self ):
+        """
+        Parse the DAG string that was instantiated in this object.
+
+        Args:
+            None
+        Return:
+            None
+        Exception:
+            ReferenceError
+            SyntaxError
+            ValueError
+        """
+        if  self._dag_str:
+            self._dag   = DAG_Parser.parse_dag( self._dag_str )
+
+            DAG_Parser.validate_dag( self._dag )
+        else:
+            raise ReferenceError( i18n( MSG_NON_DAG_INSTANCE ))
+
+    def get_parsed_dag( self ):
+        """
+        Return the aprsed DAG that was instantiated in this object.
+        You sound first executed the parse() method.
+
+        Args:
+            None
+        Return:
+            List - An intermediate representation of the parsed DAG string.
+        Exception:
+            None
+        """
+        return  self._dag
---
+++ tests/test_parser.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Edward Lau <elau1004@netscape.net>
+# Licensed under the MIT License.
+#
+"""
+DAG Parser test suite.
+
+SEE:    https://docs.python.org/3/library/unittest.html
+        https://realpython.com/python-testing/
+"""
+import  unittest
+
+from    etlite.common.lexer     import  DAG_Lexer   as  lexer
+from    etlite.common.parser    import  DAG_Parser  as  parser
+from    tests.base_test         import  BaseTest
+
+class Test_DAG_Parser( BaseTest ):
+    @classmethod
+    def setUpClass( cls ):
+        pass
+
+    @classmethod
+    def tearDownClass( cls ):
+        pass
+
+    def setUp( self ):
+        pass
+
+    def tearDown( self ):
+        pass
+
+    ####
+
+    def test_parser( self ):
+        # Positive cases.
+        cases = [
+             {"msg": "Test stripping punctuations." ,"dag": lexer.PUNCTUATION       ,"expect": []}
+            ,{"msg": "Test single character."       ,"dag": "a"                     ,"expect": ['[' ,'a']   }
+            ,{"msg": "Test multi characters."       ,"dag": "abc"                   ,"expect": ['[' ,'abc'] }
+            ,{"msg": "Test stripping embedded punc.","dag": "a!b@c"                 ,"expect": ['[' ,'abc'] }
+            ,{"msg": "Test multi entries."          ,"dag": "a,bc ,def"             ,"expect": ['[' ,'a' ,'bc','def']}
+            ,{"msg": "Test sequential jobs."        ,"dag": "[a ,b ,c]"             ,"expect": ['[' ,'a' ,'b' ,'c'] }
+            ,{"msg": "Test muti-threaded jobs."     ,"dag": "{a ,b ,c}"             ,"expect": ['{' ,'a' ,'b' ,'c'] }
+            ,{"msg": "Test muti-process jobs."      ,"dag": "(a ,b ,c)"             ,"expect": ['(' ,'a' ,'b' ,'c'] }
+            ,{"msg": "Test one nesting."            ,"dag": "[a ,{b} ,c]"           ,"expect": ['[' ,'a' ,['{','b'] ,'c']   }
+            ,{"msg": "Test two nesting."            ,"dag": "[a ,{b} ,(c)]"         ,"expect": ['[' ,'a' ,['{','b'] ,['(' ,'c']]    }
+            ,{"msg": "Test two level nesting."      ,"dag": "[a ,{b  ,(c)}]"        ,"expect": ['[' ,'a' ,['{','b'  ,['(' ,'c']]]   }
+            ,{"msg": "Test three level nesting."    ,"dag": "{ a ,b  ,[d,(e),f]}"   ,"expect": ['{' ,'a' ,'b' ,['[' ,['(' ,e] ,f]]  }
+        ]
+
+        for case in cases:
+            self.assertEquals( parser.parse_dag( case['dag'] ,case['expect'] ,case['msg'] ))
+
+if "__main__" == __name__:
+    unittest.main()
