56aa044d87a949f889eb2f5917a42ccfc03ca2f3
Edward Lau <elau1004@users.noreply.github.com>
2020-04-19 11:00:46 -0700

    Clean up some formatting

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -22,31 +22,30 @@ class TestLexer( BaseTest ):
     
     def test_next_token( self ):
         cases = [
-            {"msg": "Test stripping punctuations." ,"dag": "~"                           ,"expect": []                                              }
-           ,{"msg": "Test single character."       ,"dag": "a"                           ,"expect": ['a']                                           }
-           ,{"msg": "Test multi characters."       ,"dag": "abc"                         ,"expect": ['abc']                                         }
-           ,{"msg": "Test stripping embedded punc.","dag": "a!b@c"                       ,"expect": ['abc']                                         }
-           ,{"msg": "Test multi entries."          ,"dag": "a,bc ,def"                   ,"expect": ['a','bc','def']                                }  #expected 'bc '
-           ,{"msg": "Test sequential jobs."        ,"dag": "[a ,b ,c]"                   ,"expect": [ '[','a','b','c',']' ]                         }
-           ,{"msg": "Test muti-threaded jobs."     ,"dag": "{a ,b ,c}"                   ,"expect": [ '{','a','b','c', '}' ]                        }
-           ,{"msg": "Test muti-process jobs."      ,"dag": "(a ,b ,c)"                   ,"expect": [ '(', 'a', 'b', 'c', ')' ]                     }
-           ,{"msg": "Test one nesting."            ,"dag": "[a ,{b},c]"                  ,"expect": [ '[','a', '{', 'b' , '}' ,'c', ']' ]           }   #needed to remove space after }
-           ,{"msg": "Test two nesting."            ,"dag": "[a ,{b},(c)]"                ,"expect": [ '[', 'a', '{','b','}', '(', 'c', ')' ,']' ]   } #needed to remove space after }
-           ,{"msg": "Test two level nesting."      ,"dag": "[a ,{b,  (c)}]"              ,"expect": [ '[', 'a', '{', 'b', '(', 'c' ,')' ,'}' ,']']  }
-           ,{"msg": "Test three level nesting."    ,"dag": "{ a ,b  ,[d,(e),f]}"         ,"expect": [ '{','a','b','[','d','(','e',')','f',']','}' ] }
+            {"msg": "Test stripping punctuations." ,"dag": "~"                  ,"expect": []                                              }
+           ,{"msg": "Test single character."       ,"dag": "a"                  ,"expect": ['a']                                           }
+           ,{"msg": "Test multi characters."       ,"dag": "abc"                ,"expect": ['abc']                                         }
+           ,{"msg": "Test stripping embedded punc.","dag": "a!b@c"              ,"expect": ['abc']                                         }
+           ,{"msg": "Test multi entries."          ,"dag": "a,bc ,def"          ,"expect": ['a','bc','def']                                }  #expected 'bc '
+           ,{"msg": "Test sequential jobs."        ,"dag": "[a ,b ,c]"          ,"expect": [ '[','a','b','c',']' ]                         }
+           ,{"msg": "Test muti-threaded jobs."     ,"dag": "{a ,b ,c}"          ,"expect": [ '{','a','b','c', '}' ]                        }
+           ,{"msg": "Test muti-process jobs."      ,"dag": "(a ,b ,c)"          ,"expect": [ '(', 'a', 'b', 'c', ')' ]                     }
+           ,{"msg": "Test one nesting."            ,"dag": "[a ,{b},c]"         ,"expect": [ '[','a', '{', 'b' , '}' ,'c', ']' ]           }   #needed to remove space after }
+           ,{"msg": "Test two nesting."            ,"dag": "[a ,{b},(c)]"       ,"expect": [ '[', 'a', '{','b','}', '(', 'c', ')' ,']' ]   } #needed to remove space after }
+           ,{"msg": "Test two level nesting."      ,"dag": "[a ,{b,  (c)}]"     ,"expect": [ '[', 'a', '{', 'b', '(', 'c' ,')' ,'}' ,']']  }
+           ,{"msg": "Test three level nesting."    ,"dag": "{ a ,b  ,[d,(e),f]}","expect": [ '{','a','b','[','d','(','e',')','f',']','}' ] }
         ]
     
         for case in cases:
-            strPos=0
-            strLen=len(case['dag'])
+            strPos= 0
+            strLen= len(case['dag'])
             token = 'dag'
-            result = []
+            result= []
             while token:
-                token, strPos = lexer.next_token( case['dag'] ,strPos=strPos ,strLen=strLen )
-                if token:
+                token ,strPos = lexer.next_token( case['dag'] ,strPos=strPos ,strLen=strLen )
+                if  token:
                     result.append( token ) 
-            self.assertEqual( result ,case['expect'] )
+            self.assertEqual( result ,case['expect'] ,case['msg'] )
 
 if __name__ == '__main__':
-        
-        unittest.main()
+    unittest.main()
